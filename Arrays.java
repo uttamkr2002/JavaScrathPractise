bd14c9e17eba772e81ca07f10db1adc38124f36a
uttamkr2002 <sonuttam2002@gmail.com>
2023-01-22 16:49:23 +0530

    odified

--- Arrays.java
+++ Arrays.java
@@ -6,38 +6,28 @@ public static void inser_array(int arr1[],int pos){
     Scanner sc =new Scanner(System.in);
     int element=sc.nextInt();
     System.out.println("element");
-
-    for(int i=0;i<arr1.length;i++){
+    int size=arr1.length;
+     System.out.println("enter the size"+ size);
+    for(int i=size-2;i>=pos-1;i--){
+                arr1[i+1]=arr1[i];
+    }    
+    arr1[pos-1]=element; 
+    size=size+1;
+    for(int i=0;i<size;i++){
         System.out.println(arr1[i]);
-    }
-    for(int i=arr1.length-1;i>=pos-1;i--){
-        System.out.println(arr1[i]);
-
-
-
-        arr1[i+1]=arr1[i];
-       // arr1[pos-1]=element;
-    }
-    System.out.println("why not?");
-    arr1[pos-1]=element;
-    System.out.println(element+ ""+ pos);
-    for(int i=0;i<arr1.length+1;i++){
-        System.out.println(arr1[i]);
-    }
-    
+    }    
 }
 
 
    public static void main(String[]args){
     Scanner sc =new Scanner(System.in);
+    System.out.println("enter the size");
     int size1= sc.nextInt();
     int a1[]=new int[size1];
+     System.out.println("enter the array");
     for(int i=0;i<size1;i++){
         a1[i]=sc.nextInt();
     }
-System.out.println("welcome");
-     inser_array(a1,2);
-     //inser_array(a1, size1);
-
-   } 
+    inser_array(a1, 4);
+}
 }
