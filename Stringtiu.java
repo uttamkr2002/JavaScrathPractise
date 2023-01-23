41467ab63d7e74388b8b65944a6bf15f98d4110c
uttamkr2002 <sonuttam2002@gmail.com>
2023-01-23 16:46:50 +0530

    odified sub string finder

--- Stringtiu.java
+++ Stringtiu.java
@@ -7,14 +7,16 @@ class Stringtiu {
         int N = s2.length();  // main string
  
         /* A loop to slide pat[] one by one */
-        for (int i = 0; i <= N - M; i++) {
+        for (int i = 0; i <= N ; i++) {
+            int k=i;
             int j;
  
             /* For current index i, check for
             pattern match */
             for (j = 0; j < M; j++){
-                if (s2.charAt(i + j) != s1.charAt(j)) //??i+j 0+0 =g!,e!,e!,k!,s!,//,f=,j=0,o=o,j=1,j=2
+                if (s2.charAt(k ) != s1.charAt(j)) //??i+j 0+0 =g!,e!,e!,k!,s!,//,f=,j=0,o=o,j=1,j=2
                     break;
+                    k++;
             }
             if (j == M)
                 return i;
@@ -27,7 +29,7 @@ class Stringtiu {
     public static void main(String args[])
     {
         String s1 = "for";
-        String s2 = "geeksforgeeks";
+        String s2 = "geeksfgeeksforgeeks";
  
         int res = isSubstring(s1, s2);
  
